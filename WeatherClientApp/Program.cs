b934dfa1979e1cd50c5122ba1452a26630a679aa
Duy Phan <dpha@netcompany.com>
2022-01-04 14:44:49 +0100

    Add GetHumidity method

--- WeatherClientApp/Program.cs
+++ WeatherClientApp/Program.cs
@@ -1,12 +1,34 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using Newtonsoft.Json;
 
 namespace WeatherClientApp
 {
     class Program
     {
+        private static int[] zips = new int[] { 08816, 08536, 08810, 08812, 08817, 08832 };
+
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+        }
+
+        private static async IAsyncEnumerable<MainModel> GetHumidity()
+        {
+            var client = new HttpClient
+            {
+                BaseAddress = new Uri("https://localhost:44332/")
+            };
+
+            foreach (var zip in zips)
+            {
+                var response = await client.GetAsync($"weatherforecast?zip={zip}");
+                var data = await response.Content.ReadAsStringAsync();
+                var weather = JsonConvert.DeserializeObject<WeatherModel>(data);
+                weather.Main.Zip = zip;
+                yield return weather.Main;
+            }
         }
     }
 
@@ -20,5 +42,6 @@ namespace WeatherClientApp
         public decimal Temp { get; set; }
         public int Pressure { get; set; }
         public int Humidity { get; set; }
+        public int Zip { get; set; }
     }
 }
