d99fe76ad49e8d7b177893cf58e89525502818cd
Duy Phan <dpha@netcompany.com>
2022-01-04 14:15:53 +0100

    Create client as console app

---
+++ WeatherClientApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace WeatherClientApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
